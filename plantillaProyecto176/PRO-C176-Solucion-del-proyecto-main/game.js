934f31aaa01d0e1fdc16efc2db99a49f94f58050
MissRosy <88013528+MissRosy@users.noreply.github.com>
2023-05-22 15:51:54 -0600

    Add files via upload

---
+++ plantillaProyecto176/PRO-C176-Solucion-del-proyecto-main/game.js
@@ -0,0 +1,60 @@
+
+let words = [
+    {
+        "inputs": 7,
+        "category": "Deportes",
+        "word": "Ajedrez"
+    },
+    {
+        "inputs": 7,
+        "category": "Nombre de países europeos",
+        "word": "Francia"
+    },
+
+]
+
+$(document).ready(function () {
+    fillBlanks();
+})
+
+function fillBlanks() {
+    //Seleccionar una palabra aleatoria
+    
+
+    //Asegurar de que los espacios en blanco están vacíos para empezar
+    
+
+    //Con un ciclo for crear los espacios en blanco usando <span>, según el numero de inputs
+    //NOTA: Es como en la clase se crearon los input pero aquí son span
+
+    //Mostrar la pista con html según el campo category
+   
+    
+    //NOTA: A partir de aquí guíarse con la imagen de referencia
+    var gameOver=false
+    //Rellenar los espacios en blanco solo si se encuentra la coincidencia de caracteres
+    
+        //Obtener el id del botón pulsado
+        
+        //Obtener la vida
+       
+
+        //Bucle por todas las letras 
+        
+            //Recorrer todas las letras
+            
+                //Comprobar si aún queda vida y si el espacio en blanco está vacío o ya está lleno
+                
+
+                    //Llenar el espacio en blanco
+                    
+                    //Comprobar si la palabra adivinada está completa y mostrar el ganaste
+                    
+        //Comprobar si el numero de vidas es mayor que 0 y si la palabra esta incompleta o incorrecta y si el gameover aún no es true
+        //Para restar vidas
+        
+        //Comprobar si el numero de vidas es 0 y mostrar el perdiste
+        
+}
+
+    
